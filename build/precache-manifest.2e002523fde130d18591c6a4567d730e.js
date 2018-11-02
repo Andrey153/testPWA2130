d763589ff4d271dd607b721cd5ce6b12373c2df1
andrey <>
2018-11-02 22:44:16 +0300

    build

---
+++ build/precache-manifest.2e002523fde130d18591c6a4567d730e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe0ea98ee360265b5036",
+    "url": "/static/js/main.fe0ea98e.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "fe0ea98ee360265b5036",
+    "url": "/static/css/main.a3c0fc87.chunk.css"
+  },
+  {
+    "revision": "9714499d47ad62de371a4384d78977c4",
+    "url": "/index.html"
+  }
+];
