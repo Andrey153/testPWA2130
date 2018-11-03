3d7f12cdde4e71b5ea845b195ccca1f4a5a7472c
andrey <>
2018-11-03 08:53:47 +0300

    builder delet from gitignore

---
+++ build/precache-manifest.65698562e3884735e386be6153ceb65e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c99d82168850617a122b",
+    "url": "./static/js/main.c99d8216.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "./static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "c99d82168850617a122b",
+    "url": "./static/css/main.a3c0fc87.chunk.css"
+  },
+  {
+    "revision": "3a65361ab9fa7f1c35aed0a0d39d9e1c",
+    "url": "./index.html"
+  }
+];
